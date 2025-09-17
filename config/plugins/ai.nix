be890dd594dd535f7fa662b97f1ccbda741ffbcb
tombdf <thomas@hexasec.io>
2025-09-17 15:56:33 +0200

    feat(plugins): add copilot

---
+++ config/plugins/ai.nix
@@ -0,0 +1,11 @@
+{
+  plugins = {
+    copilot-lua = {
+      enable = true;
+    };
+    
+    copilot-chat = {
+      enable = true;
+    };
+  };
+}
--- config/plugins/default.nix
+++ config/plugins/default.nix
@@ -1,3 +1,3 @@
 {
-  imports = [ ./blink.nix ./bufferline.nix ./dap.nix ./dropbar.nix ./editor.nix ./git.nix ./mini.nix ./noice.nix ./lualine.nix ./lsp.nix ./snacks.nix ./trouble.nix ./which-key.nix ];
+  imports = [ ./ai.nix ./blink.nix ./bufferline.nix ./dap.nix ./dropbar.nix ./editor.nix ./git.nix ./mini.nix ./noice.nix ./lualine.nix ./lsp.nix ./snacks.nix ./trouble.nix ./which-key.nix ];
 }
